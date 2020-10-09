24447d7fd1b5cfcbb97958b5f3e99f7879e5ca3e
Mark Newman <mwnewman@umich.edu>
2020-10-08 23:09:36 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,150 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, 
+  TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+class DummyInput extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Type here"
+        />
+      </View>
+    )
+  }
+
+}
+
+class BasicInput extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentText: ''
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.currentText}</Text>
+        <TextInput
+          placeholder="Type here"
+          onChangeText={textValue => this.setState({currentText: textValue})}
+        />
+      </View>
+    )
+  }
+}
+
+class HelloName extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: ""
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Hello, {this.state.firstName} {this.state.lastName}!
+        </Text>
+        <View>
+          <Text>First Name: </Text>
+          <TextInput
+            placeholder="enter first name"
+            onChangeText={textValue => this.setState({firstName: textValue})}
+          />
+        </View>
+        <View>
+          <Text>Last Name: </Text>
+          <TextInput
+            placeholder="enter last name"
+            onChangeText={textValue => this.setState({lastName: textValue})}
+          />
+        </View>
+      </View>
+    )
+  }
+
+}
+
+class SaveApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputContents: '',
+      savedContents: ''
+    };
+  }
+
+  handleChange = (text) => {
+    this.setState({inputContents: text});
+  } 
+
+  handlePress = () => {
+    this.setState({savedContents: this.state.inputContents});
+  }
+
+  render() {  
+    return (
+      <View style={styles.container}>
+        <Text>
+          {this.state.savedContents}
+        </Text>
+        <TextInput
+          onChangeText={this.handleChange}
+          placeholder="Type here to translate!"
+          style={styles.input}
+        />
+        <TouchableOpacity
+          onPress={this.handlePress}
+          style={styles.button}>
+            <Text>Save</Text>
+          </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    padding: 50
   },
+
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 40,
+    width: '95%'
+  },
+  button: {
+    margin: 40,
+    height: 40,
+    width: 80,
+    backgroundColor: 'pink',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 10
+  }
 });
+
+
+//export default DummyInput;
+//export default BasicInput;
+export default HelloName;
+//export default SaveApp;
